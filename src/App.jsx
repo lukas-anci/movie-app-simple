995a4a0f7d688adec6985dff52a922307d392523
Lukas <lukas.ancikevicius@gmail.com>
2021-06-11 09:26:00 +0300

    CleanUp for fresh start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>App.jsx</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
