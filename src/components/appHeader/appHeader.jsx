69d5dff69cd2c3f6dc227c68f8f2f2bface31440
Lukas <lukas.ancikevicius@gmail.com>
2021-06-11 10:52:19 +0300

    get current time done

--- src/components/appHeader/appHeader.jsx
+++ src/components/appHeader/appHeader.jsx
@@ -4,11 +4,15 @@ import heroImg from './hero.jpg';
 import './style.css';
 class AppHeader extends Component {
   state = {};
+  getDate() {
+    const now = new Date();
+    return now.toLocaleString();
+  }
   render() {
     return (
       <header>
         <img className="hero" src={heroImg} alt="ocean" />
-        <p className="hero-date">{new Date().toLocaleDateString()}</p>
+        <p className="hero-date">{this.getDate()}</p>
       </header>
     );
   }
