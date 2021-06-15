7bb5c014de616c419ab8df1efda657cf851ac48e
Lukas <lukas.ancikevicius@gmail.com>
2021-06-15 13:53:08 +0300

    all good all works

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AboutPage from './components/newComponents/about';
 import ContactPage from './components/newComponents/contact';
 import NavbarPage from './components/newComponents/navbar';
 import { Route, Switch, Redirect } from 'react-router';
+import NotFound from './components/newComponents/404';
 
 function App() {
   return (
@@ -13,9 +14,11 @@ function App() {
       <div className="container">
         <Switch>
           <Route path="/about" component={AboutPage}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
           <Route path="/contact" component={ContactPage}></Route>
           <Route path="/about" component={AboutPage}></Route>
           <Route path="/" exact component={HomePage}></Route>
+          <Redirect to="/not-found"></Redirect>
         </Switch>
       </div>
     </div>
